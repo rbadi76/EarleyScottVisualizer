ce37ea4ff9ce29ef75dcc8edb04ce69548587777
rbadi76 <rbadi76@gmail.com>
2022-07-06 11:31:04 +0000

    Added classes and changed Backus-Naur form handling. We take in BN but grammar will not be on that form.

--- server/earley-scott.js
+++ server/earley-scott.js
@@ -1,55 +1,71 @@
-class EarleyScott{
+class EarleyScott
+{
     constructor(tokens, alphabet, grammar){
         this.tokens = tokens;
         this.alphabet = alphabet;
-        this.grammar = grammar;
+        this.grammar = [];
         this.sigmaN = new Set();        // The set of all terminals and non-terminals
         this.nonTerminals = new Set();  // The set of all non-terminals
         this.terminals = new Set();     // The set of all terminals
 
-        this.startProductions = this.grammar.filter(production => production[0] == 'S')
+        // Convert the JSON grammar to our grammar form (an array of Production objects)
+        grammar.forEach(jsonProduction => {
+            // We allow Backus-Naur form from the client but transform them to separate productions.
+            let fullRhs_BackusNaur = jsonProduction.slice(1, jsonProduction.length);
+            fullRhs_BackusNaur.forEach(rhs => {
+                this.grammar.push(new Production(jsonProduction[0], rhs));
+            });  
+        });
+
+        this.startProductions = this.grammar.filter(production => production.getLhs() == 'S')
 
         this.grammar.forEach(production => {
-            let lhs = production[0];
-            this.nonTerminals.add(lhs);
-            this.sigmaN.add(lhs);
+            this.nonTerminals.add(production.getLhs());
+            this.sigmaN.add(production.getLhs());
 
-            let rhs_arr = production.slice(1, production.length);
-            rhs_arr.forEach(rhs => {
-                let components = rhs.split(" ");
-                components.forEach(component => {
-                    this.sigmaN.add(component);
-                });
-            });
+            let rhs_components = production.getRhs().split(" ");
+            rhs_components.forEach(rhs_component => {
+                this.sigmaN.add(rhs_component);
+            });    
+
         });
 
         this.terminals = this.difference(this.sigmaN, this.nonTerminals);
 
-        this.E = [];        // Earley sets initialized
+        this.E = [];        // Earley sets initialized, an array of arrays of EarleyScottItem-objects: E[0], E[1] ... E[N]
         this.R = [];        // Queue R initialized
         this.Qmarked = [];  // Queue Q' initialized
         this.V = [];        // Queue V initialized       
     }
 
     parse(){
-        // Allow for Backus-Naur form with a vertical choice bar |. There can therefor be more than one lhs-productions.
+
         this.startProductions.forEach(startProduction => {
-            let rhs_arr = startProduction.slice(1, startProduction.length);
-            rhs_arr.forEach(rhs => {
-                let rhs_0 = rhs.split(" ")[0];
-                // According to Scott ΣN is the set of all terminals and non-terminals. Since this set can be infinite and is in the theoretical realm
-                // we simply check if the first token is a non-terminal
-                if(this.nonTerminals.has(rhs_0)){ // THIS IS NOT WORKING - CHECK WHY TOMORROW.
-                    if(this.E.length == 0)
-                    {
-                        this.E.push([['S', '· ' + rhs]]);
-                    }
-                    else
-                    {
-                        this.E[0].push(['S', '· ' + rhs]);
-                    }
+
+            let rhs_0 = startProduction.rhs.split(" ")[0];
+
+            // Create an item that might be used in the next two if-statements.
+            let esItem = new EarleyScottItem(['S', '· ' + startProduction.rhs], 0, null);
+
+            // According to Scott ΣN is the set of all terminals and non-terminals. Since this set can be infinite and is in the theoretical realm
+            // we simply check if the first token is a non-terminal
+            if(this.nonTerminals.has(rhs_0))
+            {
+                if(this.E.length == 0)
+                {
+
+                    this.E.push([esItem]);
                 }
-            });
+                else
+                {
+                    this.E[0].push(esItem);
+                }
+            }
+
+            if(this.tokens[0] == rhs_0)
+            {
+                this.Qmarked.push(esItem);
+            }
         });
     }
 
@@ -59,6 +75,50 @@ class EarleyScott{
             _difference.delete(elem)
         }
         return _difference
+    }
+}
+
+class EarleyScottItem
+{
+    constructor(production, i, w)
+    {
+        this.production = production;
+        this.i = i;
+        this.w = w;
+    }
+
+    getProduction()
+    {
+        return this.production;
+    }
+
+    get_i()
+    {
+        return this.i;
+    }
+
+    get_w()
+    {
+        return this.w;
+    }
+}
+
+class Production
+{
+    constructor(lhs, rhs)
+    {
+        this.lhs = lhs;
+        this.rhs = rhs;
+    }
+
+    getLhs()
+    {
+        return this.lhs;
+    }
+
+    getRhs()
+    {
+        return this.rhs;
     }
 }
 
