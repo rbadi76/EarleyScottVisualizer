2326711285c8ce0bf6c5976ba7f75389da26fbdf
rbadi76 <rbadi76@gmail.com>
2022-07-27 17:29:01 +0000

    Changes to look and some prelim javascript.

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,22 @@
 window.esvServiceUrl = 'http://localhost:3000'
 
+const helpTextAlphabetWords = "Words with spaces between";
+const helpTextAlphabetLetters = "Letters with spaces";
+const helpTextTokensWords = helpTextAlphabetWords;
+const helpTextTokensLetters = "Letters without spaces";
+const helpTextGrammarLetters = "Create grammar on the form S => ab|aA with linebreaks between"
+                                + " each production. RHS are characters.";
+const helpTextGrammarWords = "Create grammar on the form S => word1 word2 | word2 A with"
+                             + " linebreaks between each production. RHS are words and"
+                             + " single uppercase characters are non-terminals if found on LHS" 
+                             + " of a production.";
+
+window.addEventListener("load", () => {
+    document.getElementById("alphabet").setAttribute("placeholder", helpTextAlphabetLetters);
+    document.getElementById("tokens").setAttribute("placeholder", helpTextTokensLetters);
+    document.getElementById("grammar").setAttribute("placeholder", helpTextGrammarLetters);
+}, false);
+
 function testConnection()
 {
     //Perform an AJAX POST request to the url
@@ -20,7 +37,59 @@ function testConnection()
         });
 }
 
+function sendParseRequest()
+{
+    const alphabetStr = document.getElementById("alphabet").value;
+    const tokenStr = document.getElementById("tokens").value;
+    const grammarText = document.getElementById("grammar").value;
+    const areWords = document.getElementById("areWords").checked;
+
+    reqObj = new RequestObject(alphabetStr, tokenStr, grammarText, areWords);
+
+    console.log(JSON.stringify(reqObj));
+}
+
 function showErrorMessage(message)
 {
     alert(message);
+}
+
+function handleChange(checkbox)
+{
+    if(checkbox.checked == true)
+    {
+        document.getElementById("alphabet").setAttribute("placeholder", helpTextAlphabetWords);
+        document.getElementById("tokens").setAttribute("placeholder", helpTextTokensWords);
+        let grammarInput = document.getElementById("grammar");
+        grammarInput.setAttribute("placeholder", helpTextGrammarWords);
+        grammarInput.style.height = "8em";
+    }
+    else
+    {
+        document.getElementById("alphabet").setAttribute("placeholder", helpTextAlphabetLetters);
+        document.getElementById("tokens").setAttribute("placeholder", helpTextTokensLetters);
+        let grammarInput = document.getElementById("grammar");
+        grammarInput.setAttribute("placeholder", helpTextGrammarLetters);
+        grammarInput.style.height = "4em";
+    }
+}
+
+class RequestObject
+{
+    constructor(alphabet, tokenStr, grammarText, areWords)
+    {
+        this.alphabet = alphabet.split(" ");
+
+        if(areWords)
+        {
+            this.tokenString = tokenStr.split(" ");
+            this.grammar = grammarText.split(" ");
+        }
+        else
+        {
+            this.tokenString = tokenStr.split("");
+            this.grammar = grammarText.split("");
+        }
+
+    }
 }
